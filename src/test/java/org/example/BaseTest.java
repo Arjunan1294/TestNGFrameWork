9cd10f642134baca69a90b02a662166701bc8dd4
Arjunan <g.arjun94@gmail.com>
2023-09-29 19:19:18 +0530

    updats

--- src/test/java/org/example/BaseTest.java
+++ src/test/java/org/example/BaseTest.java
@@ -10,17 +10,17 @@ public class BaseTest {
     @Parameters({"browserName"})
     @BeforeMethod(alwaysRun = true)
     public void setup(@Optional("chrome") String browserName){
-      /*  driverFactory = new DriverFactory();
+        driverFactory = new DriverFactory();
         driver =  driverFactory.initDriver(browserName);
         DriverFactory.getDriver().get("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
-    */
+
     }
 
 
 
     @AfterMethod(alwaysRun = true)
     public void tearDown(){
-        //driver.quit();
+        driver.quit();
     }
 
 
